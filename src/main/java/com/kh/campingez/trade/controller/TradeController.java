69ae4e73b0ec61fd8b7ffa6bcb9ce6af1dffba41
dalopyomyun <sduuu59@naver.com>
2022-09-01 02:57:38 +0900

    승원 중고거래 trade 관련 기본 세팅!

---
+++ src/main/java/com/kh/campingez/trade/controller/TradeController.java
@@ -0,0 +1,93 @@
+package com.kh.campingez.trade.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kh.campingez.common.CampingEzUtils;
+import com.kh.campingez.trade.model.dto.Trade;
+import com.kh.campingez.trade.model.dto.TradePhoto;
+import com.kh.campingez.trade.model.service.TradeService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/trade")
+public class TradeController {
+		
+	@Autowired
+	ServletContext application;
+	
+	@Autowired
+	private TradeService tradeService;
+	
+	@GetMapping("/tradeList.do")
+	public void tradeList(@RequestParam(defaultValue = "1") int cPage, Model model, HttpServletRequest request) {
+//		// 1. content영역
+		Map<String, Integer> param = new HashMap<>();
+		int limit = 10;
+		param.put("cPage", cPage);
+		param.put("limit", limit);
+		List<Trade> list = tradeService.selectTradeList(param);
+//		log.debug("list = {}", list);
+		model.addAttribute("list", list);
+		
+		
+		// 2. pagebar영역
+		int totalContent = tradeService.getTotalContent();
+		log.debug("totalContent = {}", totalContent);
+		String url = request.getRequestURI(); // /spring/board/boardList.do
+		String pagebar = CampingEzUtils.getPagebar(cPage, limit, totalContent, url);
+		model.addAttribute("pagebar", pagebar);
+	}
+	
+	@GetMapping("/tradeView.do")
+	public void tradeView(@RequestParam int no, Model model) {
+		Trade trade = tradeService.selectTradeByNo(no);
+		model.addAttribute("trade", trade);
+	}
+	
+	@PostMapping("tradeEnroll.do")
+	public String tradeEnroll(Trade trade,
+							@RequestParam(name = "upFile") List<MultipartFile> upFileList,
+							RedirectAttributes redirectAttr) throws IllegalStateException, IOException {
+		
+		for(MultipartFile upFile : upFileList) {
+			if(!upFile.isEmpty()) {
+				String saveDirectory = application.getRealPath("/resources/upload/trade");
+				String renamedFilename = CampingEzUtils.getRenamedFilename(upFile.getOriginalFilename());
+				File destFile = new File(saveDirectory, renamedFilename);
+				upFile.transferTo(destFile);
+				
+				TradePhoto photo = new TradePhoto(upFile.getOriginalFilename(), renamedFilename);
+				trade.add(photo);
+			}
+			
+			
+		}
+		
+		int result = tradeService.insertTrade(trade);
+		
+		redirectAttr.addFlashAttribute("msg", "게시글 등록 완료");
+		
+		return "redirect:/trade/tradeList.do";
+	}
+	
+	
+}
---
+++ src/main/java/com/kh/campingez/trade/model/dto/Trade.java
@@ -0,0 +1,32 @@
+package com.kh.campingez.trade.model.dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trade extends TradeEntity {
+	private int photoCount;
+	private List<TradePhoto> photos = new ArrayList<>();
+	
+	public Trade(String tradeNo, String userId, String categoryId, String trade_title,
+			String trade_content, LocalDate trade_date, int readCount, int price, String trade_success,
+			TradeQuality tradeQuality, int like_count, int photoCount) {
+		super(tradeNo, userId, categoryId, trade_title, trade_content, trade_date, readCount, price, trade_success,
+				tradeQuality, like_count);
+		this.photoCount = photoCount;
+	}
+	
+	public void add(TradePhoto attach) {
+		this.photos.add(attach);
+	}
+	
+}
